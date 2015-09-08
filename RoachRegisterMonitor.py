3d1640f2a4d3515138ee9e199ebef1092bccd532
james-smith-za[ska-dell-laptop-mintRebecca] <smi.james.th@gmail.com>
2015-09-08 10:38:15 +0200

    Scroll area feature now working properly.

--- RoachRegisterMonitor.py
+++ RoachRegisterMonitor.py
@@ -1,53 +1,30 @@
 import sys
 from PyQt4 import QtGui, QtCore
 
-from RoachWidgets import RoachRegisterWidget, RoachLoaderWidget
+from RoachWidgets import RoachRegisterWidget, RoachLoaderWidget, RoachRegisterMonitor
 
 import casperfpga
 
-class RoachRegisterMonitor(QtGui.QMainWindow):
+class myWindow(QtGui.QMainWindow):
+
     def __init__(self):
-        super(RoachRegisterMonitor, self).__init__()
+        super(myWindow, self).__init__()
 
-        self.setLayout(QtGui.QVBoxLayout())
+        self.RRMWidget = RoachRegisterMonitor()
 
-        self.roachLoader = RoachLoaderWidget()
-        self.layout().addWidget(self.roachLoader)
+        self.scrollArea = QtGui.QScrollArea()
+        self.scrollArea.setWidget(self.RRMWidget)
+        self.scrollArea.setWidgetResizable(True)
 
-        self.connectButton = QtGui.QPushButton("Connect to ROACH", self)
-        self.layout().addWidget(self.connectButton)
+        self.setCentralWidget(self.scrollArea)
 
-        QtCore.QObject.connect(self.connectButton, QtCore.SIGNAL("clicked()"), self.connectToRoach)
-
-    def connectToRoach(self):
-        self.fpga = casperfpga.katcp_fpga.KatcpFpga(str(self.roachLoader.lineEdit.text()))
-        self.fpga.get_system_information(self.roachLoader.fpgFile)
-
-        regNameList = [reg for reg in dir(self.fpga.registers) if reg[0] != '_']
-        self.registerList = [getattr(self.fpga.registers, reg) for reg in regNameList if type(getattr(self.fpga.registers, reg)) == casperfpga.register.Register]
-
-        self.widgetList = []
-
-        for reg in self.registerList:
-            regValue = reg.read()
-            if regValue["data"].has_key("reg"):
-                newReg = RoachRegisterWidget(reg, "reg")
-                self.widgetList.append(newReg)
-                self.layout().addWidget(self.widgetList[-1])
-            else:
-                for key in regValue["data"].iterkeys():
-                    newReg = RoachRegisterWidget(reg, key)
-                    self.widgetList.append(newReg)
-                    self.layout().addWidget(self.widgetList[-1])
-
-
-
+        self.setGeometry(300, 300, 600, 600)
 
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
-    myWindow = RoachRegisterMonitor()
-    myWindow.show()
+    window = myWindow()
+    window.show()
     sys.exit(app.exec_())
 
 
--- RoachWidgets.py
+++ RoachWidgets.py
@@ -97,3 +97,46 @@ class RoachLoaderWidget(QtGui.QWidget):
         self.fpgFile = QtGui.QFileDialog.getOpenFileName(self, 'Open FPG file', '.')
         self.fpgName.setText(self.fpgFile)
 
+
+class RoachRegisterMonitor(QtGui.QWidget):
+    def __init__(self):
+        super(RoachRegisterMonitor, self).__init__()
+
+        self.setLayout(QtGui.QVBoxLayout())
+
+        self.roachLoader = RoachLoaderWidget()
+        self.layout().addWidget(self.roachLoader)
+
+        self.connectButton = QtGui.QPushButton("Connect to ROACH", self)
+        self.layout().addWidget(self.connectButton)
+
+        self.filterLineEdit = QtGui.QLineEdit()
+        self.filterLineEdit.setText("Filtering feature not yet implemented")
+        self.filterLineEdit.setDisabled(True)
+        self.layout().addWidget(self.filterLineEdit)
+
+        QtCore.QObject.connect(self.connectButton, QtCore.SIGNAL("clicked()"), self.connectToRoach)
+
+    def connectToRoach(self):
+        self.fpga = casperfpga.katcp_fpga.KatcpFpga(str(self.roachLoader.lineEdit.text()))
+        self.fpga.get_system_information(self.roachLoader.fpgFile)
+
+        regNameList = [reg for reg in dir(self.fpga.registers) if reg[0] != '_']
+        self.registerList = [getattr(self.fpga.registers, reg) for reg in regNameList if type(getattr(self.fpga.registers, reg)) == casperfpga.register.Register]
+
+        self.widgetList = []
+
+        for reg in self.registerList:
+            regValue = reg.read()
+            if regValue["data"].has_key("reg"):
+                newReg = RoachRegisterWidget(reg, "reg")
+                self.widgetList.append(newReg)
+                self.layout().addWidget(self.widgetList[-1])
+            else:
+                for key in regValue["data"].iterkeys():
+                    newReg = RoachRegisterWidget(reg, key)
+                    self.widgetList.append(newReg)
+                    self.layout().addWidget(self.widgetList[-1])
+
+
+
